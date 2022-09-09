957a247f77f2bc20784453972ce9e23dfc763022
zbl14 <ifthereisoneday@gmail.com>
2022-09-09 08:31:30 -0700

    adjust template

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import InvListControl from "./InvListControl";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <InvListControl />
+    </React.Fragment>
+  );
+};
+
+export default App;
