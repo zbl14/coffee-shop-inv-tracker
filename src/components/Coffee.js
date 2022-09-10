6e42c42ec20b4121538d44612a195970f044ddf4
zbl14 <ifthereisoneday@gmail.com>
2022-09-10 00:20:22 -0700

    remove commented out code

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,37 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// class Coffee extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: this.props.coffee,
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <div
-//           onClick={() => {
-//             this.props.whenCoffeeClicked(this.props.id);
-//           }}
-//         >
-//           <h3>Coffee Name: {this.props.name}</h3>
-//           <h3>Origin: {this.props.origin}</h3>
-//           <h3>Roast: {this.props.roast}</h3>
-//           <h3>Stock: {this.props.stock}</h3>
-//           <h3>Price: {this.props.price}</h3>
-//           <button onClick={() => this.props.sellCoffee(this.props.id)}>
-//             Sell 1-pound serving
-//           </button>
-//           <hr />
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 const Coffee = (props) => {
   return (
     <React.Fragment>
